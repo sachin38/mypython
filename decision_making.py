3dcd0e0a7f4b570fff239cff19a3d774a9782f5d
sachin38 <sachin.adhikari38@gmail.com>
2015-11-28 13:03:52 +0545

    intial commit

---
+++ decision_making.py
@@ -0,0 +1,8 @@
+age = raw_input("What is your age?")
+age = int(age)
+if age >= 18:
+    print "You are above 18"
+elif age <= 18:
+    print "You are under 18"
+else:
+    print "I don't know"
---
+++ ex29.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print "Too many cats"
+
+if people > cats:
+    print "Too many peeples"
+
+if people < dogs:
+    print "Too many dogs"
+
+if people > dogs:
+    print "Too many peoples"
+
+
+dogs += 5
+if people >= dogs:
+    print "People are more than equal to dogs."
+
+if people <= dogs:
+    print "People are less than or equal to dogs"
+
+if people == dogs:
+    print "People are dogs."
