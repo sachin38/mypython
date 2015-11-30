9a51ae25c0556c34f989a20206cdffe4ec2d7376
sachin38 <sachin.adhikari38@gmail.com>
2015-11-30 08:14:01 +0545

    intial commit

---
+++ beautiful.py
@@ -0,0 +1,17 @@
+"""
+If anybody's reading this then understand there is nothing personal in this program.
+I get bored and declare and execute weird functions in this way.
+"She" I used in this function is just imaginary.
+"""
+
+def beautiful():  #declaration of function... empty parenthesis means function takes no arguments
+    print "She's beautiful" #this is what function does... print this string
+
+print "She talks to me because"
+beautiful() # here function is called and executed
+print "I like her because"
+beautiful() # here function is called and executed
+
+print "I am her because"
+beautiful() # here function is called and executed
+
